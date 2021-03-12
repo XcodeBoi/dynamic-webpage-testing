5babc2a9a3f137a3b791c334fa469dc40f45d8f1
textEditUser <runningzoomcat@gmail.com>
2021-03-12 12:16:29 +1100

    heroku setup

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
 		res.render("index");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("server started on port 3000");
 });
