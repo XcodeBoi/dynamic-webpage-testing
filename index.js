7ce286d39d1a94421a4856887405876e54524056
textEditUser <runningzoomcat@gmail.com>
2021-03-15 12:24:39 +1100

    added an api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const axios = require("axios");
 const app = express();
+var jsonFile = require("./jsontesting.json");
+const fs = require("fs")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
@@ -22,6 +24,17 @@ app.post("/login", (req, res) => {
   } else {
     res.render("failure");
   }
+});
+
+app.get("/apiTest", (req, res, next) => {
+	jsonFile.idk = jsonFile.idk + 1;
+	res.json(jsonFile.idk);
+	console.log(jsonFile.idk);
+	fs.writeFile("./jsontesting.json", JSON.stringify(jsonFile), function writeJSON(err) {
+	  if (err) return console.log(err);
+	  console.log(JSON.stringify(jsonFile));
+	  console.log('writing to ' + jsonFile);
+	});
 });
 
 app.get("/repos", async (req, res) => {
